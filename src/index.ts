ef2fba56489203b7ad44ce2fce0c96f45e59ba4b
William Hayr <contact@igalaxy.dev>
2020-08-05 15:20:11 -0700

    Add highscore

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,32 @@ import Game from "./scripts/Game";
 const canvas: HTMLCanvasElement = document.getElementById("game") as HTMLCanvasElement;
 
 const score: HTMLElement = document.getElementById("score") as HTMLElement;
+const highScore: HTMLElement = document.getElementById("highScore") as HTMLElement;
 const startButton: HTMLElement = document.getElementById("startButton") as HTMLElement;
+
+if(!localStorage.getItem("highScore")) localStorage.setItem("highScore", "0");
+
+let highScoreValue: Number = Number(localStorage.getItem("highScore"));
+highScore.innerHTML = `Highscore: ${highScoreValue}`;
 
 startButton.addEventListener('click', (): void => {
     const game = new Game(canvas);
 
     startButton.style.display = "none";
+    highScore.style.display = "none";
 
-    game.on("score", s => score.innerHTML = `Score: ${s}`);
+    game.on("score", s => {
+        score.innerHTML = `Score: ${s}`;
+        if(s > highScoreValue) highScoreValue = s;
+        localStorage.setItem("highScore", `${highScoreValue}`);
+    });
+    
     game.on("over", s => {
         score.innerHTML = `You died!\n Score: ${s}`;
         startButton.innerHTML = "Restart";
         startButton.style.display = "inline-block";
+        highScore.style.display = "inline-block";
+        highScore.innerHTML = `Highscore: ${highScoreValue}`;
     });
 
     score.innerHTML = "Score: 0";
